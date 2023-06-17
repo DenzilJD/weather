df0b72276ff4d7feca1da92f7d5b6bf9130da718
Denzil Jude Daniels <denziljudedaniels@gmail.com>
2023-06-18 03:09:17 +0530

    Fixed Forecast wrong time issue

--- src/Components/Forecast.js
+++ src/Components/Forecast.js
@@ -5,8 +5,10 @@ export const Forecast = (props) => {
     console.log(weather);
     function forec() {
         console.log(weather.finalWeather.list);
+        const timezone=weather.finalWeather.city.timezone;
         return weather.finalWeather.list.map((item, index) => {
-            let date = new Date(item.dt * 1000).toUTCString();
+            let date = new Date((item.dt+timezone) * 1000).toUTCString();
+            console.log(date);
             let subfore;
             if (index % 2 === 0)
                 subfore = 'sub-forecast-even';
@@ -14,11 +16,11 @@ export const Forecast = (props) => {
                 subfore = 'sub-forecast-odd';
             let dorn = 'day';
             if (weather.finalWeather.sys.sunrise <= item.dt && weather.finalWeather.sys.sunset >= item.dt)
-                dorn = 'night';
-            else
                 dorn = 'day';
+            else
+                dorn = 'night';
             return <div className={subfore} key={item.dt}>
-                {date[date.length-]}
+                {date.substring(date.length-12, date.length-7)}
                 <WeatherIcon dorn={dorn} weather={item.weather[0].main} />
                 <h3 className='fore'>{`${Math.round(item.main.temp - 273.15)}°C`}</h3>
             </div>
